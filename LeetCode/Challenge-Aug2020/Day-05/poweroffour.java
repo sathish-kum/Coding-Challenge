12a2697b74682435dc0ed9a9d0e7a8766c6a786f
sathish-kum <48301386+sathish-kum@users.noreply.github.com>
2020-08-05 12:03:55 +0530

    Update poweroffour.java

--- LeetCode/Challenge-Aug2020/Day-05/poweroffour.java
+++ LeetCode/Challenge-Aug2020/Day-05/poweroffour.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isPowerOfFour(int num) {
         if(num == 1)
-            return true;
+            return true;// 4 power 0 == 1 which should return true
         else
         while(num >= 4)
         {
             if(num==4)
                 return true;
-            if(num%4 != 0)
+            if(num%4 != 0)//not a multiple of 4 it is false
                 return false;
             num = num/4;
         }
