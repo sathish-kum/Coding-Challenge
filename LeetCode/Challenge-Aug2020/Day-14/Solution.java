61b04fa5c86fe0b2fc583898ba080c3c7f41c1fa
sathish-kum <48301386+sathish-kum@users.noreply.github.com>
2020-08-15 10:02:45 +0530

    Update Solution.java

--- LeetCode/Challenge-Aug2020/Day-14/Solution.java
+++ LeetCode/Challenge-Aug2020/Day-14/Solution.java
@@ -3,22 +3,22 @@ class Solution {
     
         int even = 0, lodd = 0, flag = 0;
              int[] a =  new int[128];
-             for(char c : s.toCharArray())
+             for(char c : s.toCharArray())//addig the cout of each letters
                a[c]++;
              
-             for(int d : a)
+             for(int d : a)//fidig the largest odd 
                  if(d%2 == 1 && lodd < d)
                      lodd = d;
              for(int e : a)
              {
-                 if(e%2 == 0)
+                 if(e%2 == 0)//addig all the evem
                      even=  even + e;
                  else if(e%2 ==1)
                  {
                      if(e == lodd && flag == 0)
-                         flag  = 1;
+                         flag  = 1;//omittig the lodd
                      else
-                         even=  even + (e-1);
+                         even=  even + (e-1);//addig each odd-1
                  }
              }
         return even+lodd;
